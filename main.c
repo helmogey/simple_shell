cbf2151f70df5f05d4579fdaecc76f0a39d37cb4
helmogey <h.elmogey@gmail.com>
2023-09-17 13:31:15 +0300

    first

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+/**
+*main - start
+*Description: main
+*Return: void
+*/
+
+int main()
+{
+char *cmd;
+
+cmd = our_shell();
+printf("%s\n", cmd);
+return (1);
+}
+
+
+
+
+
---
+++ our_shell.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+/**
+*main - start
+*Description: main
+*Return: void
+*/
+char* our_shell(void)
+{
+char *cmd = "ls";
+return (cmd);
+}
