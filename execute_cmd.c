d27022ac59df0dbc956d510cd2851ff9d9389a39
helmogey <h.elmogey@gmail.com>
2023-09-19 23:20:45 +0300

    adding env

--- execute_cmd.c
+++ execute_cmd.c
@@ -23,7 +23,7 @@ int exec_shell(const char *prompt)
 	return (status);
 }
 */
-
+/*
 void exec_shell(const char* command) {
     pid_t pid = fork();
 
@@ -54,4 +54,86 @@ void exec_shell(const char* command) {
         }
     }
 }
+
+*/
+
+
+/*
+void exec_shell(const char* command) {
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        fprintf(stderr, "Fork failed\n");
+        exit(1);
+    } else if (pid == 0) {
+        if (command[0] == 'c' && command[1] == 'd' && command[2] == ' ') {
+            const char* directory = command + 3;
+            int result = chdir(directory);
+            exit(result == 0 ? 0 : 1);
+        } else {
+            int result = system(command);
+            exit(result);
+        }
+    } else {
+        int status;
+        pid_t waitResult = waitpid(pid, &status, 0);
+
+        if (waitResult == -1) {
+            fprintf(stderr, "Failed to wait for child process\n");
+            exit(1);
+        }
+
+          if (WIFSIGNALED(status)) {
+            int signalNumber = WTERMSIG(status);
+            printf("Child process terminated by signal: %d\n", signalNumber);
+        }
+    }
+}
+
+*/
+
+
+
+extern char** environ;
+
+void exec_shell(const char* command) {
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        fprintf(stderr, "Fork failed\n");
+        exit(1);
+    } else if (pid == 0) {
+        if (command[0] == 'c' && command[1] == 'd' && command[2] == ' ') {
+            const char* directory = command + 3;
+            int result = chdir(directory);
+            exit(result == 0 ? 0 : 1);
+        } else if (command[0] == 'e' && command[1] == 'n' && command[2] == 'v' && command[3] == '\0') {
+            char** env = environ;
+            while (*env != NULL) {
+                printf("%s\n", *env);
+                env++;
+            }
+            exit(0);
+        } else {
+            int result = system(command);
+            exit(result);
+        }
+    } else {
+        int status;
+        pid_t waitResult = waitpid(pid, &status, 0);
+
+        if (waitResult == -1) {
+            fprintf(stderr, "Failed to wait for child process\n");
+            exit(1);
+        }
+
+        if (WIFSIGNALED(status)) {
+            int signalNumber = WTERMSIG(status);
+            printf("Child process terminated by signal: %d\n", signalNumber);
+        }
+    }
+}
+
+
+
 
